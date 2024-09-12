17b3c7159a4dc32008baf551aa91c873cfa0d005
ivalukyan <ivalukyanov@gmail.com>
2024-09-12 17:29:13 +0300

    add db

---
+++ env.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Postgres:
+    def __init__(self) -> None:
+        self.db = os.getenv('POSGRES_DB')
+        self.user = os.getenv('POSTGRES_USER')
+        self.password = os.getenv('POSTGRES_PASSWORD')
+        self.host = os.getenv('POSTGRES_HOST')
+        self.port = os.getenv('POSTGRES_PORT')
